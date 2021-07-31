71ca46de3ec727bef588687b793b9ad0544a2b3f
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-07-31 23:48:34 +0530

    added: checking equality

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -16,7 +16,25 @@ public class LineComparision {
 			int y2 = SC.nextInt();
 
 			double lengthLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+			
+			System.out.println("Enter the values of next co-ordinates ");
+			System.out.print("Enter the value of (x3,y3)= ");
+			int x3 = SC.nextInt();
+			int y3 = SC.nextInt();
+			System.out.print("Enter the value of (x4,y4)= ");
+			int x4 = SC.nextInt();
+			int y4 = SC.nextInt();
+
+			double lengthLine2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+
 			System.out.println("Length of Line 1 = " + lengthLine1);
+			System.out.println("Length of Line2= " + lengthLine2);
+
+			if (lengthLine1 == lengthLine2) {
+				System.out.println("Two Lines are Equal");
+			} else {
+				System.out.println("Two Lines are Not Equal");
+			}
 		}
 	}
 
